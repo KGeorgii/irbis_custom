ec8accaf07fdc11a41affa8567d97229012e0029
KGeorgii <87044534+KGeorgii@users.noreply.github.com>
2022-03-27 23:53:17 -0700

    Update irbis_parser.py

--- irbis_parser.py
+++ irbis_parser.py
@@ -1,43 +1,70 @@
 import requests
 from bs4 import BeautifulSoup
- 
-urls = 'https://i.irklib.ru/cgi/irbis64r_61/cgiirbis_64.exe?C21COM=F&P21DBN=IBIS&I21DBN=IBIS'
+import csv
+import pandas as pd
+
+#url for the main page with calendar and years/months selectors
+urls = 'http://www.irbis-nbuv.gov.ua/cgi-bin/irbis_lib/cgiirbis_64.exe?C21COM=F&I21DBN=BIBL&P21DBN=BIBL&S21FMT=&S21ALL=&Z21ID='
 grab = requests.get(urls)
 soup = BeautifulSoup(grab.text, 'html.parser')
 
-#https://i.irklib.ru/cgi/irbis64r_61/cgiirbis_64.exe?C21COM=F&P21DBN=IBIS&I21DBN=IBIS // The first page when we enter the electronic catalogue. First, I tried to run through the "YEARS" selector
-#https://i.irklib.ru/cgi/irbis64r_61/cgiirbis_64.exe?Z21ID=&I21DBN=IBIS&P21DBN=IBIS&C21COM=F&LNG=&S21YEAR=2021 // when we select other year, the links gets like this.
-#https://i.irklib.ru/cgi/irbis64r_61/cgiirbis_64.exe?C21COM=F&P21DBN=IBIS&I21DBN=IBIS&S21YEAR=2021 //The first link with added &S21YEAR=2022 works!
+#years are filled dynamically through js script, hardcoded them here
+year_list_selector = ['2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998', '1997', '1996', '1995', '1994', '1993']
 
-#looking for year in selector
-year_list = soup.find(class_='inp1')
-#select a year
-year_list_selector = year_list.find_all('option')
-#count number of years for a loop
-x = len(year_list_selector)
-
-#print(year_list.prettify())
-#print(x)
-
-#empty list to fill
+#first loop – creating urls for each year, output in sitemap.csv
 pages = []
-
 #looping through the years
 for years in year_list_selector:
-   year = years.contents[0]
-   #print(year)
-   url = 'https://i.irklib.ru/cgi/irbis64r_61/cgiirbis_64.exe?Z21ID=&I21DBN=IBIS&P21DBN=IBIS&C21COM=F&LNG=&S21YEAR=' + year
+   print("year is", years)
+   url = 'http://www.irbis-nbuv.gov.ua/cgi-bin/irbis_lib/cgiirbis_64.exe?Z21ID=&I21DBN=BIBL&P21DBN=BIBL&C21COM=F&S21YEAR=' + years
    pages.append(url)
-   #print(url)
+   print(url)
    grab = requests.get(url)
    soup = BeautifulSoup(grab.text, 'html.parser')
-   f = open("sitemap.xml", "a")
+   f = open("sitemap.csv", "a")
    for link in soup.find_all("a"):
       data = link.get('href')
       f.write(data)
       f.write("\n")
-      #print(data)
- 
-   f.close()
+      print(data)
+f.close()
 
-#print(pages)
+#second loop – creating urls for each month within a year, output in months_sitemap.csv
+searchfile = open("sitemap.csv", "r")
+months_ds = open("months_sitemap.csv", "w")
+for line in searchfile:
+    if "S21All=<.>DP" in line:
+        print(line)
+        months_ds.write('http://www.irbis-nbuv.gov.ua' + line)
+searchfile.close()
+months_ds.close()
+
+#third loop – iterating through urls inside months entry, output in content.csv
+results = []
+with open('months_sitemap.csv', newline='') as f:
+    for row in csv.reader(f):
+        print("row is", row)
+        row = str(row)[2:-2]
+        print("row is", row)
+        grab = requests.get(row)
+        soup = BeautifulSoup(grab.text, 'html.parser')
+        ds = open ("content.csv", "a")
+        for link in soup.find_all("a"):
+            data = link.get('href')
+            ds.write(data)
+            ds.write("\n")
+            print(data)
+print(results)
+f.close()
+
+#fourth loop – refining results of content.csv: removing duplicate links and links to third-party resources, adding 'http://www.irbis-nbuv.gov.ua' prefix. Output in content_cleaned.csv
+csv = open("content.csv", "r")
+cleaned_csv = open("content_cleaned.csv", "w")
+for line in csv:
+    if "/cgi-bin/irbis_lib" in line:
+        cleaned_csv.write('http://www.irbis-nbuv.gov.ua' + line)
+csv.close()
+cleaned_csv.close()
+df = pd.read_csv('content_cleaned.csv')
+df.drop_duplicates(inplace=True)
+df.to_csv('content_cleaned.csv', index=False)
